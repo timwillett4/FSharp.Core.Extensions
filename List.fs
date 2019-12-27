2dd9f697761445c2c3cc0908eb11adddaca5a70d
Tim Willett <timwillett4@gmail.com>
2019-12-26 21:38:10 -0800

    Add Lists module

---
+++ List.fs
@@ -0,0 +1,53 @@
+﻿// code taken from https://fsharpforfunandprofit.com/posts/elevated-world-4/#traverse 
+// with only slight modifications
+
+[<AutoOpen>]
+module FSharp.Core.Extensions.Lists
+
+/// Map a Result producing function over a list to get a new Result 
+/// using applicative style
+/// ('a -> Result<'b>) -> 'a list -> Result<'b list>
+let rec traverseResultA f list =
+
+    // define the applicative functions
+    let (<*>) = Result.apply
+    let retn = Result.Ok
+
+    // define a "cons" function
+    let cons head tail = head :: tail
+
+    // loop through the list
+    match list with
+    | [] -> 
+        // if empty, lift [] to a Result
+        retn []
+    | head::tail ->
+        // otherwise lift the head to a Result using f
+        // and cons it with the lifted version of the remaining list
+        retn cons <*> (f head) <*> (traverseResultA f tail)
+
+
+/// Map a Result producing function over a list to get a new Result 
+/// using monadic style
+/// ('a -> Result<'b>) -> 'a list -> Result<'b list>
+let rec traverseResultM f list =
+
+    // define the monadic functions
+    let (>>=) x f = Result.bind f x
+    let retn = Result.Ok
+
+    // define a "cons" function
+    let cons head tail = head :: tail
+
+    // loop through the list
+    match list with
+    | [] -> 
+        // if empty, lift [] to a Result
+        retn []
+    | head::tail ->
+        // otherwise lift the head to a Result using f
+        // then lift the tail to a Result using traverse
+        // then cons the head and tail and return it
+        f head                 >>= (fun h -> 
+        traverseResultM f tail >>= (fun t ->
+        retn (cons h t) ))
--- Result.fs
+++ Result.fs
@@ -1,64 +1,66 @@
-﻿namespace FSharp.Core.Extensions
+﻿// code taken from: https://github.com/swlaschin/13-ways-of-looking-at-a-turtle/blob/master/Common.fsx
+// with only slight modifications
 
-module Result =
+[<AutoOpen>]
+module FSharp.Core.Extensions.Result
 
-    open System
+open System
 
-    let returnR x = 
-        Ok x
+let returnR x = 
+    Ok x
 
-    // infix version of bind
-    let ( >>= ) xR f = 
-        Result.bind f xR
+// infix version of bind
+let ( >>= ) xR f = 
+    Result.bind f xR
 
-    // infix version of map
-    let ( <!> ) = Result.map 
+// infix version of map
+let ( <!> ) = Result.map 
 
-    let applyR fR xR = 
-        fR >>= (fun f ->
-        xR >>= (fun x ->
-            returnR (f x) ))
+let apply fR xR = 
+    fR >>= (fun f ->
+    xR >>= (fun x ->
+        returnR (f x) ))
 
-    // infix version of apply
-    let ( <*> ) = applyR
+// infix version of apply
+let ( <*> ) = apply
 
-    // lift a one-parameter function to result world (same as mapR)
-    let lift1R f x = f <!> x 
+// lift a one-parameter function to result world (same as mapR)
+let lift f x = f <!> x 
 
-    // lift a two-parameter function to result world
-    let lift2R f x y = f <!> x <*> y
+// lift a two-parameter function to result world
+let lift2 f x y = f <!> x <*> y
 
-    /// Computation Expression
-    type ResultBuilder() =
-        member this.Bind(m:Result<'a,'error>,f:'a -> Result<'b,'error>) = 
-            Result.bind f m
-        member this.Return(x) :Result<'a,'error> = 
-            returnR x
-        member this.ReturnFrom(m) :Result<'a,'error> = 
-            m
-        member this.Zero() :Result<unit,'error> = 
-            this.Return ()
-        member this.Combine(m1, f) = 
-            this.Bind(m1, f)
-        member this.Delay(f) = 
-            f
-        member this.Run(m) = 
-            m()
-        member this.TryWith(m:Result<'a,'error>, h: exn -> Result<'a,'error>) =
-            try this.ReturnFrom(m)
-            with e -> h e
-        member this.TryFinally(m:Result<'a,'error>, compensation) =
-            try this.ReturnFrom(m)
-            finally compensation()
-        member this.Using(res:#IDisposable, body) : Result<'b,'error> =
-            this.TryFinally(body res, (fun () -> match res with null -> () | disp -> disp.Dispose()))
-        member this.While(cond, m) =
-            if not (cond()) then 
-                this.Zero()
-            else
-                this.Bind(m(), fun _ -> this.While(cond, m))
-        member this.For(sequence:seq<_>, body) =
-            this.Using(sequence.GetEnumerator(),
-                (fun enum -> this.While(enum.MoveNext, fun _ -> body enum.Current)))
+/// Computation Expression
+type ResultBuilder() =
+    member this.Bind(m:Result<'a,'error>,f:'a -> Result<'b,'error>) = 
+        Result.bind f m
+    member this.Return(x) :Result<'a,'error> = 
+        returnR x
+    member this.ReturnFrom(m) :Result<'a,'error> = 
+        m
+    member this.Zero() :Result<unit,'error> = 
+        this.Return ()
+    member this.Combine(m1, f) = 
+        this.Bind(m1, f)
+    member this.Delay(f) = 
+        f
+    member this.Run(m) = 
+        m()
+    member this.TryWith(m:Result<'a,'error>, h: exn -> Result<'a,'error>) =
+        try this.ReturnFrom(m)
+        with e -> h e
+    member this.TryFinally(m:Result<'a,'error>, compensation) =
+        try this.ReturnFrom(m)
+        finally compensation()
+    member this.Using(res:#IDisposable, body) : Result<'b,'error> =
+        this.TryFinally(body res, (fun () -> match res with null -> () | disp -> disp.Dispose()))
+    member this.While(cond, m) =
+        if not (cond()) then 
+            this.Zero()
+        else
+            this.Bind(m(), fun _ -> this.While(cond, m))
+    member this.For(sequence:seq<_>, body) =
+        this.Using(sequence.GetEnumerator(),
+            (fun enum -> this.While(enum.MoveNext, fun _ -> body enum.Current)))
         
-    let result = ResultBuilder()
+let result = ResultBuilder()
