d87cfc5e79a4844805e9dd68a432630475bf204e
Tim Willett <timwillett4@gmail.com>
2022-02-08 22:59:32 -0800

    Add rule validation builder

---
+++ Validation.fs
@@ -0,0 +1,14 @@
+﻿module FSharp.Core.Extensions.Validation
+
+type ValidationRule<'T> = 'T -> bool * string
+
+let buildValidator (rulesList : ValidationRule<'T> list) =
+    rulesList |> List.reduce (fun firstRule secondRule ->
+        fun objToValidate -> 
+            match (objToValidate |> firstRule) with
+            | true,_ ->
+                match objToValidate |> secondRule with
+                | true,_ -> true, ""
+                | false,error -> false, error
+            | false, error -> false, error 
+        )
