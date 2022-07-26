453952fb65a2822f18327b63494146f2b3f0dcfe
Tim Willett <timwillett4@gmail.com>
2022-07-26 00:59:17 -0700

    Refactor tryStartGame

--- FSharpType.fs
+++ FSharpType.fs
@@ -2,6 +2,7 @@
 
 open Microsoft.FSharp.Reflection
 
+// This method currently only works for unions where all cases have no data
 let getAllUnionCases<'T>() =
     [for case in FSharpType.GetUnionCases(typeof<'T>) -> FSharpValue.MakeUnion(case, [||]) |> unbox<'T>]
 
